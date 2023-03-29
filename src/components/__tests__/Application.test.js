0c3caaeadbfad5064f6432ae4325c8d862f64e4a
tessfbs <tessfbs@gmail.com>
2023-03-28 22:52:16 -0400

    create mock module

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent, waitForElement } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -9,6 +9,28 @@ afterEach(cleanup);
 //To skip a test, use xit or test.skip:
 
 
-it("renders without crashing", () => {
-  render(<Application />);
+// it("renders without crashing", () => {
+//   render(<Application />);
+// });
+
+// it("defaults to Monday and changes the schedule when a new day is selected", () => {
+//   const { getByText } = render(<Application />);
+
+//   return waitForElement(() => getByText("Monday"))
+//     .then(() => {
+//       fireEvent.click(getByText("Tuesday"));
+//       expect(getByText("Leopold Silvers")).toBeInTheDocument();
+
+//     });
+// });
+
+it("changes the schedule when a new day is selected", async () => {
+  const { getByText } = render(<Application />);
+
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
+
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -16,18 +16,18 @@ export default function useApplicationData() {
 
   //Requesting data from the API
   useEffect(() => {
-    const GET_DAYS = "http://localhost:8001/api/days"
-    const GET_APPOINTMENTS = "http://localhost:8001/api/appointments"
-    const GET_INTERVIEWERS = "http://localhost:8001/api/interviewers"
+    const GET_DAYS = "/api/days"
+    const GET_APPOINTMENTS = "/api/appointments"
+    const GET_INTERVIEWERS = "/api/interviewers"
 
     Promise.all([
       axios.get(GET_DAYS),
       axios.get(GET_APPOINTMENTS),
       axios.get(GET_INTERVIEWERS),
     ]).then((all) => {
-      console.log('DAYS', all[0].data);
-      console.log('APPOINTMENTS', all[1].data);
-      console.log('INTERVIEWERS', all[2].data);
+      // console.log('DAYS', all[0].data);
+      // console.log('APPOINTMENTS', all[1].data);
+      // console.log('INTERVIEWERS', all[2].data);
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     })
     /*empty dependency because API request does not depend on the state or props of thus component => never rerun this request */
