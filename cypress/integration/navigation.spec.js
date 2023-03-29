82ca40417ff038c1b3f73bc574854e29abf1faaf
tessfbs <tessfbs@gmail.com>
2023-03-29 17:30:22 -0400

    add navigation test with cypress

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,27 @@
+
+
 describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
+  });
+});
+
+describe("should navigate to Tuesday", () => {
+  it("should visit root", () => {
+
+    cy.visit("/");
+
+    // cy.contains("li", "Tuesday")
+    //   .click()
+    //   .should("have.css", "background-color", "rgb(242, 242, 242)");
+
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected")
+
+
+
+
+
   });
 });
