669d4fd58fe8fceec5ea0041966fe9a4a858d14a
tessfbs <tessfbs@gmail.com>
2023-03-30 21:39:49 -0400

    clean up

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -9,7 +9,7 @@ describe("Appointments", () => {
     cy.contains("Monday");
   });
 
-  xit("should book an interview", () => {
+  it("should book an interview", () => {
 
     // Clicks on the "Add" button in the second appointment
     cy.get("[alt=Add]")
@@ -18,11 +18,11 @@ describe("Appointments", () => {
 
     // // Enters their name
     cy.get(".appointment__create-input")
-      .type("Lydia Miller-Jones")
+      .type("Lydia Miller-Jones");
 
     // // Chooses an interviewer
     cy.get("[alt='Sylvia Palmer']")
-      .click()
+      .click();
 
     // // Clicks the save button
     cy.contains("Save")
@@ -34,41 +34,36 @@ describe("Appointments", () => {
 
   });
 
-  xit("should edit an interview", () => {
+  it("should edit an interview", () => {
 
     // Clicks the edit button for the existing appointment
     cy.get("[alt=Edit]")
-      .click({ force: true })
+      .first()
+      .click({ force: true });
 
     // Changes the name and interviewer
-    cy.get("[data-testid=student-name-input]")
-      .clear()
-      .type("Lydia Miller-Jones");
-
-    cy.get("[alt='Tori Malcolm']")
-      .click()
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
 
     // Clicks the save button
-    cy.contains("Save")
-      .click();
-
+    cy.contains("Save").click();
 
     // Sees the edit to the appointment
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Tori Malcolm");
 
-
   });
 
-  it("should cancel an interview", () => {
+  xit("should cancel an interview", () => {
 
     // Clicks the delete button for the existing appointment
     cy.get("[alt=Delete]")
-      .click({ force: true })
+      .first()
+      .click({ force: true });
 
     // Clicks the confirm button
-    cy.contains("button", "Confirm")
-      .click()
+    cy.contains("Confirm").click();
+
 
     // Sees that the appointment slot is empty
 
@@ -81,8 +76,6 @@ describe("Appointments", () => {
     //check that the .appointment__card--show element that contains the text "Archie Cohen" should not exist.
     cy.contains(".appointment__card--show", "Archie Cohen")
       .should("not.exist");
-
-
 
   });
 
